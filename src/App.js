7f2d879e1029b6355b3d005432b19b055d0f9c19
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2019-10-04 00:38:13 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Logo from './assets/logo.svg';
+import Routes from './routes';
+
+function App() {
+  return (
+    <div className="container">
+      <img src={Logo} alt="Aircnc" />
+
+      <div className="content">
+        <Routes />
+      </div>
+    </div>
+  );
+}
+
+export default App;
